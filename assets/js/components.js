bf9fb7700b1af7fe7202d3baf7c602e35e55ed52
Egor Borisovsky <egor@intervolga.ru>
2020-03-12 00:45:53 +0400

    Добавлен компонент левой формы. Оставлены заглушки под отправку запросов на апи

--- assets/js/components.js
+++ assets/js/components.js
@@ -1,7 +1,85 @@
+Vue.component('left-form', {
+    props: ['methods'],
+    data: function () {
+        return {
+            importFilePath: '',
+            testPercent: 75,
+            selectedMethod: 0
+        }
+    },
+    computed: {
+        methodsForSelect: function () {
+            const methods = this.methods;
+            return Object.keys(this.methods).map(key => ({value: key, text: methods[key].name}));
+        }
+    },
+    watch: {
+        importFilePath: function (newVal) {
+            this.$root.$emit('import_file_path_changed', newVal);
+        },
+        testPercent: function (newVal) {
+            this.$root.$emit('test_percent_changed', newVal);
+        },
+        selectedMethod: function (newVal) {
+            this.$root.$emit('selected_method_changed', newVal);
+        }
+    },
+    template: '#template-left-form',
+    methods: {
+        onTrainHandler: function() {
+            this.$root.$emit('do_train');
+        },
+        onPredictHandler: function() {
+            this.$root.$emit('do_predict');
+        }
+    }
+});
+
 var app = new Vue({
     el: '#app',
-    template: '<b-container>{{ message }}</b-container>',
-    data: {
-        message: 'Привет, Vue!'
+    data: function() {
+        return {
+            importFilePath: '',
+            testPercent: 75,
+            methods: {
+                0: {
+                    name: 'svm'
+                },
+                1: {
+                    name: 'k-ближайших соседей'
+                }
+            },
+            selectedMethod: 0,
+            apiBase: 'http://localhost:5000',
+            apiRoutes: {
+                trainData: '/train/',
+                predictData: '/predict/'
+            }
+        }
+    },
+    created: function () {
+        this.$on('import_file_path_changed', function (data) {
+            this.importFilePath = data;
+        });
+        this.$on('test_percent_changed', function (data) {
+            this.testPercent = parseInt(data);
+        });
+        this.$on('selected_method_changed', function (data) {
+            this.selectedMethod = parseInt(data);
+        });
+        this.$on('do_train', function () {
+            this.doTrain();
+        });
+        this.$on('do_predict', function () {
+            this.doPredict();
+        });
+    },
+    methods: {
+        doTrain: function () {
+            console.log('training start...');
+        },
+        doPredict: function () {
+            console.log('predicting start...');
+        }
     }
 });
